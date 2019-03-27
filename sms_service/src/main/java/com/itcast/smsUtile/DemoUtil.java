a092814fdfa93dfa55c4a5bbd1ec7b4bb413cd1b
LeonWu <190187791@qq.com>
2019-03-27 21:22:46 +0800

    蓝色被修改

--- sms_service/src/main/java/com/itcast/smsUtile/DemoUtil.java
+++ sms_service/src/main/java/com/itcast/smsUtile/DemoUtil.java
@@ -4,5 +4,6 @@ public class DemoUtil {
     public void method1(){
         System.out.println("新增功能被修改了");
         System.out.println("新增功能被修改了然后更新了");
+        System.out.println("如果该类显示蓝色则表示是被修改的");
     }
 }
