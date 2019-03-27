9828eb8c617aa5648e7ecc25ad0f5f5ec3cd49ae
LeonWu1994 <46775184+LeonWu1994@users.noreply.github.com>
2019-03-27 22:01:06 +0800

    Update DemoUtil.java

--- sms_service/src/main/java/com/itcast/smsUtile/DemoUtil.java
+++ sms_service/src/main/java/com/itcast/smsUtile/DemoUtil.java
@@ -5,5 +5,6 @@ public class DemoUtil {
         System.out.println("新增功能被修改了");
         System.out.println("新增功能被修改了然后更新了");
         System.out.println("如果该类显示蓝色则表示是被修改的");
+        System.out.println("在github上面修改了");
     }
 }
