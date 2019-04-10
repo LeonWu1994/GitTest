6468fdf1850800f2a87316331c7e6beb4dd65527
LeonWu <190187791@qq.com>
2019-04-10 15:30:28 +0800

    第四次修改

--- MyDemoGit/src/main/java/demoGit.java
+++ MyDemoGit/src/main/java/demoGit.java
@@ -2,6 +2,7 @@ public class demoGit {
     private static void method(){
         System.out.println("this is a demo git test!");
         System.out.println("第二次修改!");
+        System.out.println("第四次修改!");
     }
 
     public static void main(String[] args) {
