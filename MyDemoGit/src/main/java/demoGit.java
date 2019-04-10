01af47b1b80020bcc4841bb280d5b6c7b6ce807c
LeonWu <190187791@qq.com>
2019-04-10 16:21:06 +0800

    第八次修改

--- MyDemoGit/src/main/java/demoGit.java
+++ MyDemoGit/src/main/java/demoGit.java
@@ -6,6 +6,7 @@ public class demoGit {
         System.out.println("第五次修改!");
         System.out.println("第6次修改!");
         System.out.println("第7次修改!");
+        System.out.println("第8次修改!");
     }
 
     public static void main(String[] args) {
